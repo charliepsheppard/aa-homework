c287c2ad4b203ada62ec86192b8fbd0290c2a4ad
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-11 23:26:10 -0800

    Add queue class and methods

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -16,4 +16,20 @@ class Stack
   end
 end
 
+class Queue
+  def initialize
+    @queue = []
+  end
 
+  def enqueue(el)
+    @queue.push(el)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue[-1]
+  end
+end
