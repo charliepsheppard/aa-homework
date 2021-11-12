b8304782db4fcf3c30191a4525f0a7ee54ad5e4a
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-11 23:40:37 -0800

    Add map class and associated methods

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -33,3 +33,33 @@ class Queue
     @queue[-1]
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, val)
+    if @map.any? { |pair| pair[0] == key }
+      raise "Already exists"
+    else
+      @map << [key, val]
+    end
+  end
+
+  def get(key)
+    @map.each { |pair| return pair[1] if pair[0] == key }
+  end
+
+  def delete(key)
+    @map.each_with_index do |pair, i|
+      if pair[0] == key
+        @map.delete_at(i)
+      end
+    end
+  end
+
+  def show
+    @map[-1]
+  end
+end
