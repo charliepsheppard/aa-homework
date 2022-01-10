404f71df27c97d34a6ace190553f81ea26cd690c
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-01-09 23:22:10 -0800

    Add addRows function

--- Week11/minesweeper/components/Board.jsx
+++ Week11/minesweeper/components/Board.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Tile from './Tile';
 
-const Board = () => {
-  return (
-    <div>
-      <Tile />
-    </div>
-  );
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addRows = this.addRows.bind(this);
+    this.addTiles = this.addTiles.bind(this);
+  }
+
+  addRows() {
+    const board = this.props.board;
+    return board.grid.map((row, i) => {
+      return (
+        <div key={i} className="row">
+
+        </div>
+      );
+    })
+  }
+
+  addTiles() {
+    
+  }
 }
 
 export default Board;
